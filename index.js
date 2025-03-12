98959fb25276bc537a1ade06a4969bb66c911b09
maxhayden <maxphayden@gmail.com>
2025-03-12 11:10:11 -0300

    changed port

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import express from "express"
 
 const app = express()
 
-app.listen(5001, () => console.log("api running on port 50o1"));
+app.listen(80, () => console.log("api running on port 5001"));
 
-app.get('/store-products', (req, res) => res.json("product is being stored"));
+app.get('/store-products', (req, res) => {
+    res.json("product is being stored")
+});
 
-app.get('/list-products', (req, res) => res.json("listing all products"));
+app.get('/list-products', (req, res) => {
+    res.json("listing all products")
+});
