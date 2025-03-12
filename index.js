64d1140cea70d031de470d0140cca611ad61c36c
maxhayden <maxphayden@gmail.com>
2025-03-12 11:03:39 -0300

    added endpoints

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const app = express()
 
 app.listen(5001, () => console.log("api running on port 50o1"));
 
-app.get('/', (req, res) => res.json("the api is running"));
+app.get('/store-products', (req, res) => res.json("product is being stored"));
+
+app.get('/list-products', (req, res) => res.json("listing all products"));
