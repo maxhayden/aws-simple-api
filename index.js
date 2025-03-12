da680ec86c14d5e82fa2c2c2d8d6e7f9ec2c22af
maxhayden <maxphayden@gmail.com>
2025-03-12 11:12:48 -0300

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 
 const app = express()
 
-app.listen(80, () => console.log("api running on port 5001"));
+app.listen(3000, () => console.log("api running on port 5001"));
 
 app.get('/store-products', (req, res) => {
     res.json("product is being stored")
