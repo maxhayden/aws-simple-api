a970712a52b2db5964c32b37ffafda0baf5f04fd
maxhayden <maxphayden@gmail.com>
2025-03-12 13:50:56 -0300

    connect to db

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 import express from "express"
 
+const config = {
+    user: "admin",
+    password: "ayl3yh23",
+    server: "two-tier-app-db.cwnjetnzxoed.us-east-1.rds.amazonaws.com",
+    database: "appDB",
+  };
+  
+  app.get("/", async (req, res) => {
+      try {
+          await sql.connect(config);
+          res.send("DB Connected");
+      } catch(err)
+      {
+          res.send(err);
+      }
+  });
+  
+
 const app = express()
 
 app.listen(3000, () => console.log("api running on port 5001"));
@@ -11,3 +29,9 @@ app.get('/store-products', (req, res) => {
 app.get('/list-products', (req, res) => {
     res.json("listing all products")
 });
+
+
+const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`service is running on:: [${port}]`);
+  });
